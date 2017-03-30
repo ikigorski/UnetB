597ec3680d0a5499afac8022355ea94543d1a5bb
PhelipeSR <phelipeuni@gmail.com>
2017-03-30 18:50:57 -0300

    asdfo

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br" style="height: 100%;">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>UNetB - Qualidade de Rede</title>
+	<link rel="shortcut icon" href="imagens/favicon.png" type="image/x-icon" />
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/custom.css" rel="stylesheet">
+</head>
+ 
+<body style="height: 100%;">
+
+	<?php
+		include "public/header.inc";
+	?>	
+	
+	<div id="mapa" style="height: 100%; margin-bottom: auto;"></div>
+	<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAXumgSxD3IY_5VhkxwyEQcbBVOS55H-xQ&amp;sensor=false"></script>
+	<script src="js/mapa.js"></script>	
+
+	
+
+	<?php
+		include "public/footer.inc";
+	?>
+	
+	
+	
+	
+</body>
+<script src="js/jquery.min.js"></script>    
+<script src="js/bootstrap.min.js"></script>
+</html>
