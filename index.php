9d0cb1f9ea5eecd8afbef2e61cff5e1322d333dd
PhelipeSR <phelipeuni@gmail.com>
2017-04-04 17:12:55 -0300

    pequenas mudanças na validação de cadastro

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>UNetB - Qualidade de Rede</title>
-		<link rel="shortcut icon" href="../imagens/favicon.png" type="image/x-icon" />
+		<link rel="shortcut icon" href="imagens/favicon.png" type="image/x-icon" />
 		<link href="css/bootstrap.css" rel="stylesheet">
 		<link href="css/custom.css" rel="stylesheet">
 	</head>
--- public/sobre.php
+++ public/sobre.php
@@ -1,41 +1,41 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>UNetB - Sobre</title>
-		<link rel="shortcut icon" href="../imagens/favicon.png" type="image/x-icon" />
-		<link href="../css/bootstrap.css" rel="stylesheet">
-		<link href="../css/custom.css" rel="stylesheet">
-	</head>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>UNetB - Sobre</title>
+	<link rel="shortcut icon" href="../imagens/favicon.png" type="image/x-icon" />
+	<link href="../css/bootstrap.css" rel="stylesheet">
+	<link href="../css/custom.css" rel="stylesheet">
+</head>
 
-	<body>
+<body>
 	
-		<div class="wrapper">
+	<div class="wrapper">
 
-			<?php
-				include "header.inc";
-			?>
+		<?php
+			include "header.inc";
+		?>
 		
-			<h1>Em Construção 1</h1>
-			<h1>Em Construção 2</h1>
-			<h1>Em Construção 3</h1>
-			<h1>Em Construção 4</h1>
-			<h1>Em Construção 1</h1>
-			<h1>Em Construção 2</h1>
-			<div class="push"></div>
+		<h1>Em Construção 1</h1>
+		<h1>Em Construção 2</h1>
+		<h1>Em Construção 3</h1>
+		<h1>Em Construção 4</h1>
+		<h1>Em Construção 1</h1>
+		<h1>Em Construção 2</h1>
 
-		</div>
+		<div class="push"></div>
+	</div>
 		
-		<div class="footer">
-			<?php
-				include "../public/footer.inc";
-			?>
-		</div>
+	<div class="footer">
+		<?php
+			include "../public/footer.inc";
+		?>
+	</div>
 
-	</body>
+</body>
 	<script src="../js/jquery.min.js"></script>
 	<script src="../js/bootstrap.min.js"></script>
 </html>
