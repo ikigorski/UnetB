90baad2aedad23b15379a4153a6064c50953a6aa
PhelipeSR <phelipeuni@gmail.com>
2017-04-04 20:08:11 -0300

    Corrigido alguns bugs na validação de cadastro e adicionado uma validação para aceitar senhas apenas com 6 ou mais caracteres

--- js/valida_cadastro.js
+++ js/valida_cadastro.js
@@ -1,26 +1,33 @@
 /* Atribui ao evento submit do formulário a função de validação de dados */
 var form = document.getElementById("form-contato");
-
-if (form.addEventListener){                 
+if (form.addEventListener) {                   
     form.addEventListener("submit", validaCadastro);
+} else if (form.attachEvent) {                  
+    form.attachEvent("onsubmit", validaCadastro);
 }
 
-/* Atribui ao evento keypress do input matricula a função para formatar a matrícula ##/####### */
-var inputCPF = document.getElementById("matricula");
-if (inputCPF.addEventListener) {                   
-    inputCPF.addEventListener("keypress", function(){mascaraTexto(this, '##/#########')});
+/* Atribui ao evento keypress do input matricula a função para formatar a matricula (00/0000000) */
+var inputMatricula = document.getElementById("matricula");
+if (inputMatricula.addEventListener) {                   
+    inputMatricula.addEventListener("keypress", function(){mascaraTexto(this, '##/#######')});
+} else if (inputMatricula.attachEvent) {                  
+    inputMatricula.attachEvent("onkeypress", function(){mascaraTexto(this, '##/########')});
 }
 
 /* Atribui ao evento keypress do input data de nascimento a função para formatar o data (dd/mm/yyyy) */
 var inputDataNascimento = document.getElementById("data_nascimento");
 if (inputDataNascimento.addEventListener) {                   
     inputDataNascimento.addEventListener("keypress", function(){mascaraTexto(this, '##/##/####')});
+} else if (inputDataNascimento.attachEvent) {                  
+    inputDataNascimento.attachEvent("onkeypress", function(){mascaraTexto(this, '##/##/####')});
 }
 
 /* Atribui ao evento keypress do input celular a função para formatar o Celular (00 00000-0000) */
 var inputCelular = document.getElementById("celular");
 if (inputCelular.addEventListener) {                   
     inputCelular.addEventListener("keypress", function(){mascaraTexto(this, '## #####-####')});
+} else if (inputCelular.attachEvent) {                  
+    inputCelular.attachEvent("onkeypress", function(){mascaraTexto(this, '## #####-####')});
 }
 
 /* Atribui ao evento change do input FILE para upload da foto*/
@@ -28,6 +35,8 @@ var inputFile = document.getElementById("foto");
 var foto_cliente = document.getElementById("foto-cliente");
 if (inputFile.addEventListener) {                   
     inputFile.addEventListener("change", function(){loadFoto(this, foto_cliente)});
+} else if (inputFile.attachEvent) {                  
+    inputFile.attachEvent("onchange", function(){loadFoto(this, foto_cliente)});
 }
 
 /* Função para validar os dados antes da submissão dos dados */
@@ -35,11 +44,10 @@ function validaCadastro(evt){
 	var nome = document.getElementById('nome');
 	var email = document.getElementById('email');
 	var matricula = document.getElementById('matricula');
-	var senha = document.getElementById('senha');
-	var status = document.getElementById('status');	
+	var sexo = document.getElementById('sexo');
 	var data_nascimento = document.getElementById('data_nascimento');
 	var celular = document.getElementById('celular');
-	var filtro_email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+	var filtro = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
 	var contErro = 0;
 
 
@@ -59,53 +67,36 @@ function validaCadastro(evt){
 		caixa_email.innerHTML = "Favor preencher o E-mail";
 		caixa_email.style.display = 'block';
 		contErro += 1;
-	}else if(filtro_email.test(email.value)){
+	}else if(filtro.test(email.value)){
 		caixa_email.style.display = 'none';
 	}else{
 		caixa_email.innerHTML = "Formato do E-mail inválido";
 		caixa_email.style.display = 'block';
 		contErro += 1;
 	}
-	
-	/* Validação do campo matricula */
+
+	/* Validação do campo Matrícula*/
 	caixa_matricula = document.querySelector('.msg-matricula');
 	if(matricula.value == ""){
-		caixa_matricula.innerHTML = "Favor preencher a Matricula";
+		caixa_matricula.innerHTML = "Favor preencher a Matrícula";
 		caixa_matricula.style.display = 'block';
 		contErro += 1;
 	}else{
 		caixa_matricula.style.display = 'none';
 	}
-	
-	/* Validação do campo senha */
+
+	/* Validação do campo Senha*/
 	caixa_senha = document.querySelector('.msg-senha');
 	if(senha.value == ""){
-		caixa_senha.innerHTML = "Favor preencher a Senha";
+		caixa_senha.innerHTML = "Favor preencher a senha";
+		caixa_senha.style.display = 'block';
+		contErro += 1;
+	}else if(senha.value.length < 6){
+		caixa_senha.innerHTML = "Senha deve ter no mínimo 6 caracteres";
 		caixa_senha.style.display = 'block';
 		contErro += 1;
 	}else{
 		caixa_senha.style.display = 'none';
-	}
-	 
-
-	/* Validação do campo data de nascimento */
-	caixa_data = document.querySelector('.msg-data');
-	if(data_nascimento.value == ""){
-		caixa_data.innerHTML = "Favor preencher a Data de Nascimento";
-		caixa_data.style.display = 'block';
-		contErro += 1;
-	}else{
-		caixa_data.style.display = 'none';
-	}
-
-	/* Validação do campo cpf */
-	caixa_cpf = document.querySelector('.msg-cpf');
-	if(cpf.value == ""){
-		caixa_cpf.innerHTML = "Favor preencher o CPF";
-		caixa_cpf.style.display = 'block';
-		contErro += 1;
-	}else{
-		caixa_cpf.style.display = 'none';
 	}
 
 	/* Validação do campo celular */
@@ -118,14 +109,14 @@ function validaCadastro(evt){
 		caixa_celular.style.display = 'none';
 	}
 
-	/* Validação do campo status */
-	caixa_status = document.querySelector('.msg-status');
-	if(status.value == ""){
-		caixa_status.innerHTML = "Favor preencher o Status";
-		caixa_status.style.display = 'block';
+	/* Validação do campo sexo */
+	caixa_sexo = document.querySelector('.msg-sexo');
+	if(sexo.value == ""){
+		caixa_sexo.innerHTML = "Favor preencher o sexo";
+		caixa_sexo.style.display = 'block';
 		contErro += 1;
 	}else{
-		caixa_status.style.display = 'none';
+		caixa_sexo.style.display = 'none';
 	}
 
 	if(contErro > 0){
@@ -133,7 +124,7 @@ function validaCadastro(evt){
 	}
 }
 
-/* Função para formatar dados conforme parâmetro enviado, CPF, DATA, e CELULAR */
+/* Função para formatar dados conforme parâmetro enviado, DATA e CELULAR */
 function mascaraTexto(t, mask){
 	var i = t.value.length;
 	var saida = mask.substring(1,0);
---
+++ public/action_cliente.php
@@ -0,0 +1,3 @@
+<?php
+	echo "Cadastro funcionando";
+?>
