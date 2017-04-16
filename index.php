c7cf8c8fa793a287893758da6a9df7e51b964768
PhelipeSR <phelipeuni@gmail.com>
2017-04-16 19:44:58 -0300

    Melhorias nos links entre páginas

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>UNetB - Qualidade de Rede</title>
-		<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
+		<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon"/>
 		<link href="css/bootstrap.css" rel="stylesheet">
 		<link href="css/custom.css" rel="stylesheet">
 	</head>
  
-	<body>
+	<body> 
 	
 		<?php
 			include "public/header.inc";
