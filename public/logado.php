8549d9e5ca0a392eed2c43f1282be8a1953488bb
PhelipeSR <phelipeuni@gmail.com>
2017-04-17 10:43:45 -0300

    pequenas alterações na página logado

--- public/logado.php
+++ public/logado.php
@@ -1,6 +1,15 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="pt-br">
+
 <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>UNetB - Sobre</title>
+	<link rel="shortcut icon" href="../UNetB/images/favicon.png" type="image/x-icon" />
+	<link href="../css/bootstrap.css" rel="stylesheet">
+	<link href="../css/custom.css" rel="stylesheet">
+
 <?php
 	
 	session_start();
@@ -8,9 +17,9 @@
 
 		unset($_SESSION['email']);
 		unset($_SESSION['password']);
-		//header('location:loginInterface.php');
+		header('location:loginInterface.php');
 	}
-
+	include "header.inc";
 	$logado = $_SESSION['email'];
 ?>
 
@@ -19,9 +28,9 @@
 </head>
 
 <body>
-<table width="800" height="748" border="1">
+<table width="800" height="748" border="50">
 	<tr>
-		<td height="90" colspan="2" bgcolor="#CCCCCC">SISTEM WEB TESTE
+		<td height="90" colspan="2" bgcolor="#CCCCCC">
 		<?php 
 			echo" Bem vindo $logado";
 		?>
