cdc0aa8eb34643f266689b0a0a481625e976f820
Phelipe <phelipeuni@gmail.com>
2017-04-21 21:01:32 -0300

    retirando comflito feito pelo git

--- index.php
+++ index.php
@@ -3,24 +3,6 @@
  * Este arquivo não faz nada, apenas inclui os arquivos necessários
  */
  
-<<<<<<< HEAD
 // Config
 require_once 'config.php';
-=======
-	<body> 
-	
-		<?php
-			include "public/header.inc";
-		?>
 
-
-		<div id="map"></div>
-		
-	</body>
-
-	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAXumgSxD3IY_5VhkxwyEQcbBVOS55H-xQ&callback=initMap"></script>
-	<script src="public/js/mapa.js"></script>
-	<script src="public/js/jquery.min.js"></script>
-	<script src="public/js/bootstrap.min.js"></script>
-</html>
->>>>>>> 6fd49427479d2fe038241fab9cec445d19501952
