7ef07c119b14ae5d670fe134856564f327eae2e4
Phelipe <phelipeuni@gmail.com>
2017-05-03 12:08:02 -0300

    arrumado problema de codificação no banco de dados

--- views/login.php
+++ views/login.php
@@ -1,5 +1,6 @@
 <?php
 	header( 'Content-Type: text/html; charset=utf-8' );
+		
 	// Verifica se houve POST e se o usuário ou a senha é(são) vazio(s)
 	if (!empty($_POST) AND (empty($_POST['email']) OR empty($_POST['password']))){
 		header("Location: login-view.php"); exit;
@@ -8,13 +9,20 @@
 	// Tenta se conectar ao servidor MySQL
 	$con = mysqli_connect('127.0.0.1', 'root', '','unetb') or trigger_error(mysqli_error());
 	
+		mysqli_query($con,"SET NAMES 'utf8'");
+		mysqli_query($con,'SET character_set_connection=utf8');
+		mysqli_query($con,'SET character_set_client=utf8');
+		mysqli_query($con,'SET character_set_results=utf8');
 
 	$email = utf8_decode($_POST['email']);
 	$password = hash('sha256',utf8_decode($_POST['password']));
 
 	// Validação do usuário/senha digitados
 	$sql = "SELECT `name`,`email`, `password` FROM `user` WHERE (`email` = '". $email ."') AND (`password` = '". $password ."') LIMIT 1";
+		
 	$query = mysqli_query($con,$sql);
+
+
 	
 	if (mysqli_num_rows($query) != 1) {
 	// Mensagem de erro quando os dados são inválidos e/ou o usuário não foi encontrado
