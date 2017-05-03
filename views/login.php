ecc46da922138737c1af095d997289c443a0a957
Phelipe <phelipeuni@gmail.com>
2017-05-03 10:59:57 -0300

    Mudança na função de login

--- views/login.php
+++ views/login.php
@@ -1,20 +1,22 @@
 <?php
 
 	// Verifica se houve POST e se o usuário ou a senha é(são) vazio(s)
-	if (!empty($_POST) AND (empty($_POST['email']) OR empty($_POST['password']))) {
-  	header("Location: login-view.php"); exit;}
+	if (!empty($_POST) AND (empty($_POST['email']) OR empty($_POST['password']))){
+  		header("Location: login-view.php"); exit;
+ 	}
+	
 	// Tenta se conectar ao servidor MySQL
-	mysqli_connect('127.0.0.1', 'root', '','unetb') or trigger_error(mysqli_error());
-	// Tenta se conectar a um banco de dados MySQL
-	mysqli_select_db('unetb') or trigger_error(mysqli_error()); 
+	$con = mysqli_connect('127.0.0.1', 'root', '','unetb') or trigger_error(mysqli_error());
+	
 
 	$email = utf8_decode($_POST['email']);
 	$password = hash('sha256',utf8_decode($_POST['password']));
 
 	// Validação do usuário/senha digitados
-	$sql = "SELECT `name`, `email`, `password` FROM `user` WHERE (`email` = '". $email ."') AND (`password` = '". $password ."') LIMIT 1";
-	$query = mysqli_query($sql);
-	if (mysql_num_rows($query) != 1) {
+	$sql = "SELECT `email`, `password` FROM `user` WHERE (`email` = '". $email ."') AND (`password` = '". $password ."') LIMIT 1";
+	$query = mysqli_query($con,$sql);
+	
+	if (mysqli_num_rows($query) != 1) {
   	// Mensagem de erro quando os dados são inválidos e/ou o usuário não foi encontrado
 		require_once 'login-view.php';
 		echo"
@@ -24,16 +26,17 @@
 			</script>
 		";
 	} else {
-  	// Salva os dados encontados na variável $resultado
-  	$resultado = mysql_fetch_assoc($query);
+
+	  	// Salva os dados encontados na variável $resultado
+		$resultado = mysqli_fetch_assoc($query);
 
 
-  	// Se a sessão não existir, inicia uma
-  	if (!isset($_SESSION)) session_start();
-	$_SESSION['email'] = $resultado['email'];
- 	$_SESSION['nome'] = $resultado['name'];
+		// Se a sessão não existir, inicia uma
+		if (!isset($_SESSION)) session_start();
+		$_SESSION['email'] = $resultado['email'];
+		$_SESSION['nome'] = $resultado['name'];
 
-	header('location:http://localhost/UnetB/views/home-login-view.php');
+		header('location:http://localhost/UnetB/views/home-login-view.php');
 
 	}
 
