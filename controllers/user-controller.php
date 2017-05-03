1c5f7b64e7107b857a3f74fd4edea88795cf1f2e
Edudacosta <educostao.1050@gmail.com>
2017-05-03 11:44:44 -0300

    Após se cadastrar o usuário é redirecionado para página de boas vindas.

--- controllers/user-controller.php
+++ controllers/user-controller.php
@@ -41,11 +41,12 @@
 		$UserControl->registeruser();
 		mysqli_close($con);
 		echo "Cadastro realizado com sucesso";
+		// Se a sessão não existir, inicia uma
+		if (!isset($_SESSION)) session_start();
+		$_SESSION['email'] = $email;
+		$_SESSION['nome'] = $name;
+		header('location:../views/home-login-view.php');
+
 	}
-	
-	
-	
-	
-	//header('location:../home-login-view.php');
 
 ?>
