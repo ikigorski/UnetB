c3f4df6e9521f7d704d41be051f7f7ce36a1b24f
Phelipe <phelipeuni@gmail.com>
2017-05-04 22:21:06 -0300

    adicionado um arquivo para incluir o head

--- views/about-view.php
+++ views/about-view.php
@@ -1,15 +1,8 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="shortcut icon" href="_images/favicon.png" type="image/x-icon"/>
-		<link href="_css/bootstrap.css" rel="stylesheet">
-		<link href="_css/custom.css" rel="stylesheet">
-		<title>UnetB - Sobre</title>
-	</head>
+	<!-- INCLUE O HEAD NA PÁGINA -->
+	<?php include "_includes/head.php";?>
 
 	<body>
 
@@ -24,4 +17,5 @@
 
 	<script src="_js/jquery.min.js"></script> <!-- Carrega JS jquery-->
 	<script src="_js/bootstrap.js"></script> <!-- Carrega JS do bootstrap-->
+
 </html>
--- views/login-view.php
+++ views/login-view.php
@@ -1,15 +1,8 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">		
-		<link rel="shortcut icon" href="_images/favicon.png" type="image/x-icon"/>
-		<link href="_css/bootstrap.css" rel="stylesheet">
-		<link href="_css/custom.css" rel="stylesheet">
-		<title>Login</title>
-	</head>
+	<!-- INCLUE O HEAD NA PÁGINA -->
+	<?php include "_includes/head.php";?>
 
 	<body>
 
