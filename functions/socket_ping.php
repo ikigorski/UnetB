631929de77fb243e8138a186036ba740240e8dcd
leolamartine <leandrolamartine@yahoo.com.br>
2017-05-09 00:18:12 -0300

    adicionada função para pingar um host via socket

---
+++ functions/socket_ping.php
@@ -0,0 +1,23 @@
+<?php
+// Function to check response time
+function pingDomain($domain){
+    $starttime = microtime(true);
+    $file      = fsockopen ($domain, 80, $errno, $errstr, 10);
+    $stoptime  = microtime(true);
+    $status    = 0;
+ 
+    if (!$file) $status = -1;  // Site is down
+    else {
+        fclose($file);
+        $status = ($stoptime - $starttime) * 1000;
+       // $status = floor($status);
+    }
+    return $status . ' ms';
+}
+ echo pingDomain("www.google.com") . '<br>';
+ echo pingDomain("www.facebook.com") . '<br>';
+ echo pingDomain("www.globoesporte.com");
+
+
+
+?>
