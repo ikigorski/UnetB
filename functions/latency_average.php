b8926c37c030cea089dbfaf25e1f44924e57ccce
leolamartine <leandrolamartine@yahoo.com.br>
2017-05-09 00:58:18 -0300

    alteração  no código do socket_ping (agora latency_average) para calcular o tempo médio de resposta

---
+++ functions/latency_average.php
@@ -0,0 +1,28 @@
+<?php
+// Function to check response time -- vulgo latência
+function pingDomain($domain){
+    $starttime = microtime(true); 
+    $file      = fsockopen ($domain, 80, $errno, $errstr, 10);
+    $stoptime  = microtime(true);
+    $status    = 0;
+ 
+    if (!$file) $status = -1;  // Site is down
+    else {
+        fclose($file);
+        $status = ($stoptime - $starttime) * 1000;
+        $status = floor($status); // função para arredondar o valor numérico
+    }
+    return $status . ' ms';
+}
+
+$pings = array (); //criando um array para armazenar os resultados de pingDomain
+
+for ( $i = 0; $i < 100; $i++) //laço for para preencher o array com 100 pings
+{
+   $pings[$i] = pingDomain("www.google.com"); // pingando um domínio qualquer
+}
+
+$media  = array_sum ($pings) / sizeof($pings); //aqui somamos todos os pings e dividimos pela quantidade de pings (tamanho do array)
+echo $media;
+
+?>
