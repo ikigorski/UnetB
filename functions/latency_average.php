63a7d8c200927acfba27eb848464811a45c797d0
victorsanp <victorpi20@hotmail.com>
2017-05-09 22:31:20 -0300

    Funcao jitter em funcao da latencia(quase pronta)

--- functions/latency_average.php
+++ functions/latency_average.php
@@ -23,23 +23,26 @@ for ( $i = 0; $i < 100; $i++) //laço for para preencher o array com 100 pings
 }
 
 $media  = array_sum ($pings) / sizeof($pings); //aqui somamos todos os pings e dividimos pela quantidade de pings (tamanho do array)
-echo $media. '<br>';
+echo 'Latência: ';
+echo $media ' ms'. '<br>';
 
+//CALCULANDO JITTER(desvio padrao da latencia)
 
 $jitter = array();
 
 for($i = 0; $i < 100; $i++)
 {
-	$pings[$i] = $pings[$i] + "0"; //Tentando transformar os numeros de string para floats
+	$valor_ping = number_format(floatval($pings[$i])); //Tentando transformar os numeros de string para floats
 
-    $num = pow(($pings[$i] - $media) , 2); //calculando um dos termos da variancia
+    $num = pow(($valor_ping - $media) , 2); //calculando um dos termos da variancia
 
 	$jitter[$i] = floor($num); //atribuindo ao array na posicao i o valor da variancia
 }
 
 $jitter_total = sqrt(array_sum($jitter) / 100);//tirando raiz da soma de variancias dividida pelo numero de termos somados -- vulgo calculando desvio padrao = jitter
 
-echo $jitter_total;
+echo 'Jitter: ';
+echo round($jitter_total) . '<br>';
 
 ?>
 
