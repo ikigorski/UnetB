e81095313426f65a646f04db6c0f58edae877a1d
Edudacosta <educostao.1050@gmail.com>
2017-05-16 20:43:28 -0300

    Trabalhando nas configurações do mapa

--- functions/latency_average.php
+++ functions/latency_average.php
@@ -10,7 +10,7 @@ function pingDomain($domain){
     else {
         fclose($file);
         $status = ($stoptime - $starttime) * 1000;
-        $status = floor($status); // função para arredondar o valor numérico
+        $status = floor($status); // função para arredonda
     }
     return $status . ' ms';
 }
--- views/_js/mapa.js
+++ views/_js/mapa.js
@@ -1,7 +1,44 @@
-var map;
+// var map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: -15.763, lng: -47.869},
+//     zoom: 18
+//   });
+// }
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -15.763, lng: -47.869},
-    zoom: 18
-  });
+
+  // Exibir mapa;
+  var myLatlng = new google.maps.LatLng(-15.763, -47.869);
+  var mapOptions = {
+    zoom: 18,
+    center: myLatlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+
+  // Exibir o mapa na div #mapa;
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+
+
+  // Marcador personalizado;
+  var image = 'https://cdn1.iconfinder.com/data/icons/gpsmapicons/blue/gpsmapicons01.png';
+  var marcadorPersonalizado = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      icon: image,
+      title: 'ICC',
+      animation: google.maps.Animation.DROP
+});
 }
+
+// Função para carregamento assíncrono
+function loadScript() {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAXumgSxD3IY_5VhkxwyEQcbBVOS55H-xQ&callback=initMap";
+  document.body.appendChild(script);
+
+}
+
+window.onload = loadScript;
