d81e8c134075f334d87317d2509a9c2f979811bc
diegommo <diego@aluno.unb.br>
2017-05-17 18:23:20 -0300

    Para pegar mais de uma amostra, função intensidade.php

--- functions/intensidade.php
+++ functions/intensidade.php
@@ -4,23 +4,18 @@ function checkPacketLoss() {
 	// Comando ping -c que retorna informações sobre pacotes de dados, como pacotes transmitidos, recebidos e perdidos;
 
     $command = 'iwconfig | grep "Signal level"';
-
+    $dados=array();
     //Executa comando na máquina para pegar a intensidade do sinal de WiFi.
-
-    $output = shell_exec($command);
-    $slevel=getSignalLevel($output);
-    echo $slevel; exit;
-    // a função preg_match retorna um valor para saber se obtivemos sucesso ou não na busca. Neste casso ela irá procurar no resultado do comando acima a porcentagem de perda de pacotes, e armazená-la na variável martch.
-
-   {
-
-        // Se houver compatibilidade na procura
-      //AQUIIIIIIIIIIIIIII ------>>>>>>  $packetLoss = (float)$match[1];
-    	//
-        throw new \Exception('Packet loss not found.');
+    for($i=1;$i<=100;$i++)
+    {
+      $output = shell_exec($command);
+      $slevel=getSignalLevel($output);
+      $dados[$i]=$slevel;
+      echo "Sinal $i: $slevel<br>";
+      usleep(50000);
     }
+    
 
-    return $packetLoss;
 }
 
 // Usaremos o servidor local para testes com contagem 40;
